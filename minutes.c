5875207d03cea47dcd217eb403ba38a63bba1daf
sandhiya812 <43338242+sandhiya812@users.noreply.github.com>
2019-02-19 12:31:27 +0530

    Create minutes.c

---
+++ minutes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int convert_to_minutes(int h, int m)
+{
+      int total_minutes;
+      total_minutes = (h * 60) + m;
+      return total_minutes;
+}
+int main()
+{
+      int hours, minutes, total_minutes;
+      printf("\nEnter the Time in Hours and Minutes:\n");
+      printf("\nHours:\t");
+      scanf("%d", &hours);
+      printf("\nMinutes:\t");
+      scanf("%d", &minutes);
+      total_minutes = convert_to_minutes(hours, minutes);
+      printf("\nTotal Time in Minutes:\t%d\n", total_minutes);
+ return 0;
+ getch();
+}
